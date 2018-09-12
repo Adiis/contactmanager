07e4d3ee815613ad66eaaaab8336697afdc65454
Adis <adis.hasovic@gmail.com>
2018-09-12 17:42:47 +0200

    28

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import "./App.css";
+import Contacts from "./componets/contacts/Contacts";
+import Header from "./componets/layout/Header";
+import AddContact from "./componets/contacts/AddContact";
+
+import { Provider } from "./context";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <div className="App">
+          <Header branding="Contact Manager" />
+          <div className="container">
+            <AddContact />
+            <Contacts />
+          </div>
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
