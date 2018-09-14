e04bf33f20b48de0c7b42da84ab71d0106aa3fa8
Adis <adis.hasovic@gmail.com>
2018-09-14 18:10:27 +0200

    33

--- src/componets/contacts/AddContact.js
+++ src/componets/contacts/AddContact.js
@@ -7,7 +7,8 @@ class AddContact extends Component {
   state = {
     name: "",
     email: "",
-    phone: ""
+    phone: "",
+    errors: {}
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
@@ -15,6 +16,21 @@ class AddContact extends Component {
   onSubmit = (dispatch, e) => {
     e.preventDefault();
     const { name, email, phone } = this.state;
+
+    // Check for errors
+
+    if (name === "") {
+      this.setState({ errors: { name: "Name is requiered" } });
+      return;
+    }
+    if (email === "") {
+      this.setState({ errors: { email: "Email is requiered" } });
+      return;
+    }
+    if (phone === "") {
+      this.setState({ errors: { phone: "Phone is requiered" } });
+      return;
+    }
 
     const newConstact = {
       id: uuid(),
@@ -27,12 +43,13 @@ class AddContact extends Component {
     this.setState({
       name: "",
       email: "",
-      phone: ""
+      phone: "",
+      errors: {}
     });
   };
 
   render() {
-    const { name, email, phone } = this.state;
+    const { name, email, phone, errors } = this.state;
 
     return (
       <Consumer>
@@ -49,6 +66,7 @@ class AddContact extends Component {
                     placeholder="Enter name"
                     value={name}
                     onChange={this.onChange}
+                    error={errors.name}
                   />
                   <TextInputGroup
                     label="Email"
@@ -57,6 +75,7 @@ class AddContact extends Component {
                     placeholder="Enter email"
                     value={email}
                     onChange={this.onChange}
+                    error={errors.email}
                   />
                   <TextInputGroup
                     label="Phone"
@@ -64,6 +83,7 @@ class AddContact extends Component {
                     placeholder="Enter phone"
                     value={phone}
                     onChange={this.onChange}
+                    error={errors.phone}
                   />
                   <input
                     type="submit"
