632f441df1508ef4cf348e247b7353fcc711d799
Adis <adis.hasovic@gmail.com>
2019-07-13 11:31:40 +0200

    Updates

---
+++ precache-manifest.2278aaf1d875cd45a5fa1f2e216b37aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "910af396a45b2697a7d25896aeff08c7",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "61b8068627a75b1cf08e",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "2c87745a86c73079e2f6",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "61b8068627a75b1cf08e",
+    "url": "/contactmanager/static/js/2.d865a23f.chunk.js"
+  },
+  {
+    "revision": "2c87745a86c73079e2f6",
+    "url": "/contactmanager/static/js/main.f58a600d.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
